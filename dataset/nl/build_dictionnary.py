486f77e63be5516dfcda36648623357177ad8cab
Nathanael BEAU <n.beau@LGOP4451.local>
2023-01-11 16:58:48 +0100

    clean vocab

--- dataset/nl/build_dictionnary.py
+++ dataset/nl/build_dictionnary.py
@@ -9,30 +9,21 @@ import json
 
 
 def main():
+    i = 4
     for filename in sys.argv[1:]:
         print('Processing', filename)
-        word_freqs = OrderedDict()
-        with open(filename, 'r', encoding='utf-8') as f:
-            for line in f:
-                words_in = line.strip().split(' ')
-                for w in words_in:
-                    if w not in word_freqs:
-                        word_freqs[w] = 0
-                    word_freqs[w] += 1
-        words = list(word_freqs.keys())
-        freqs = list(word_freqs.values())
-
-        sorted_idx = numpy.argsort(freqs)
-        sorted_words = [words[ii] for ii in sorted_idx[::-1]]
-
         worddict = OrderedDict()
         worddict['<eos>'] = 0
         worddict['<sos>'] = 1
         worddict['<unk>'] = 2
         worddict['<pad>'] = 3
-        # FIXME We shouldn't assume <EOS>, <GO>, and <UNK> aren't BPE subwords.
-        for ii, ww in enumerate(sorted_words):
-            worddict[ww] = ii+4
+        with open(filename, 'r', encoding='utf-8') as f:
+            for line in f:
+                words_in = line.strip().split(' ')
+                for w in words_in:
+                    if w not in worddict:
+                        worddict[w] = i
+                        i += 1
 
         # The JSON RFC requires that JSON text be represented using either
         # UTF-8, UTF-16, or UTF-32, with UTF-8 being recommended.
